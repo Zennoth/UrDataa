cc06c8bc03584eb33c940dffba87064356772b9d
Zennoth <62875502+Zennoth@users.noreply.github.com>
2020-12-13 21:54:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/urdataa/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.urdataa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int splash_time_out = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeintent = new Intent (SplashScreen.this, Login.class);
+                startActivity(homeintent);
+                finish();
+            }
+        },splash_time_out);
+    }
+}
